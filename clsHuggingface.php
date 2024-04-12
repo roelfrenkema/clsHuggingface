d85534d7306c58f6b23982eec451ddf7ba2b9b9d
Roelf Renkema <xs-githubby@roelfrenkema.com>
2024-04-12 15:44:11 +0200

    almost ready for first full release

--- clsHuggingface.php
+++ clsHuggingface.php
@@ -15,10 +15,17 @@ class Huggingface {
 	private $endPoint;    //containing our endpoint
 	private $models;      //models
 	private $sName;       //shortname for model
-	public $imgStore;     //path to store image
+	public $logAll;      //logging?
+	public $imgStore;     //path to store image1
+    public $slMax;        //max value for sleep before passing out
 	public $slUpdate;     // sleep incremeter
     public $exiv2;        //use exiv2
-
+    public $prompt;       //use for prompt
+	public $negative;     //use for negative prompt
+    public $exiv2User;     //get your name stamped
+    public $exiv2Copy;     //copyright info
+    public $negPrompt;     //negative prompt from user
+    
 	/*
 	* Function: __construct
 	* Input   : not applicable
@@ -36,10 +43,6 @@ class Huggingface {
 	
 
 	//check for module tidy
-		if (! extension_loaded('tidy')) {
-		  echo "PHP module tidy is needed to run clsStraico. Please install it. Exiting!\n";
-		  exit;
-		}
 		if (! extension_loaded('readline')) {
 		  echo "PHP module readline is needed to run clsStraico. Please install it. Exiting!\n";
 		  exit;
@@ -56,12 +59,21 @@ class Huggingface {
 		}
 		$this->models = json_decode(file_get_contents(__DIR__.'/models.json'), true);
 		
-		 
-		$this->endPoint = Huggingface::INFERENCE.$this->models['stablexl'];
-		$this->sName = 'stablexl';
+		foreach( $this->models as $tag => $value){
+			$this->sName = $tag;
+			$this->endPoint = Huggingface::INFERENCE.$value;
+			break;
+		}
+			 
 		$this->imgStore = '';
 		$this->slUpdate = 30;
+		$this->slMax = 300;
 		$this->exiv2 = false;
+		$this->exiv2User = 'clsHuggingface';
+		$this->exiv2Copy = 'CC BY-NC-SA 4.0';
+		$this->negPrompt = "";
+		$this->logAll = false;
+
 		
 		echo "Welcome to clsHuggingface v0.1.0 - enjoy!\n\n";
 
@@ -92,37 +104,31 @@ class Huggingface {
 		}elseif( substr($input,0,9) == "/setmodel"){
 			 $this->changeModel(substr($input,10));
 
-		// Save models	
-		}elseif( $input == "/savemodels"){
-			file_put_contents(__DIR__."models.json",json_encode($this->models));
+		// Import models	
+		}elseif( substr($input,0,11) == "/txt2models"){
+			 $this->models = array();
+			 $this->importModels(substr($input,12));
 
 		// Add model	
 		}elseif( substr($input,0,9) == "/addmodel"){
-			$granate = explode(" ",substr($input,10));
-			if(! array_key_exists($granate[0],$this->models)){
-				$smodel = array_search($granate[1], $this->models);
-				if(! $smodel){
-					$this->models[$granate[0]] = $granate[1];
-				}else{
-					echo "\nModel already available as $smodel\n";
-				}
-			}else{
-				echo "\nShortname already in use!\n";
-			}
+			
+			$this->addModel(substr($input,10));
+
 		// Remove model	
 		}elseif( substr($input,0,9) == "/delmodel"){
 			$granate = trim(substr($input,10));
 			unset($this->models[$granate]);
+			echo "\nModel removed\n";
 
-		// Save models	
-		}elseif( $input == "/getmodels"){
-			$this->models = json_decode(file_get_contents(__DIR__.'models.json'), true);
+		// Get models	
+		}elseif( substr($input,0,10) == "/getmodels"){
+			$this->getModels($input);
 
 		// list model	
 		}elseif( $input == "/listmodels"){
 
 			foreach ($this->models as $key => $value) {
-				echo "$key \n";
+				echo "$key - $value\n";
 			}
 
 		// list model	to textfile
@@ -134,6 +140,21 @@ class Huggingface {
 			$id = $this->imgStore.'/models.txt';
 			file_put_contents($id,$atext);
 			echo "List saved to $id\n";
+
+
+		// loop prompt through models	
+		}elseif( substr($input,0,5) == "/loop"){
+			$this->loopModels(substr($input,6));
+
+		// logon	
+		}elseif( $input == "/logon"){
+			$this->logAll = true;
+			echo "Log enabled.";
+
+		// logon	
+		}elseif( $input == "/logoff"){
+			$this->logAll = false;
+			echo "Log disabled.";
      
 		//  help	
 		}elseif( $input == "/helpme"){
@@ -147,7 +168,8 @@ class Huggingface {
 
 		// Proccess
 		}else{
-			$answer = $this->apiCompletion($input);
+			$this->prompt = $input;
+			$answer = $this->apiCompletion($input.",".$this->negPrompt);
 		}
 	}
 
@@ -165,9 +187,10 @@ class Huggingface {
 
 	public function apiCompletion($aiMessage){
 
-if(substr($aiMessage,0,1) == '/') return;
+		if(substr($aiMessage,0,1) == '/') return;
 
-echo "\n\nEndpoint short name: ".$this->sName."\n\n";
+		echo "\n\nEndpoint short name: ".$this->sName."\n\n";
+		if($this->logAll) $this->logString("Main: Endpoint short name: ".$this->sName) ;
 
 		$httpMethod = 'POST';
 
@@ -209,57 +232,63 @@ echo "\n\nEndpoint short name: ".$this->sName."\n\n";
 		$pointer = true;
 		
 		while ( $pointer = true){
+			
+			if( $timer > $this->slMax ){
+				echo "\nModel skipped due to sleeptimeout, sorry.\n";
+				if($this->logAll) $this->logString("Main: Model skipped due to sleeptimeout, sorry.\n") ;
+				return $this->slMax;
+			}
+			
 			sleep($timer);
 			
 			// Communicate
 			$result = @file_get_contents($this->endPoint, false, $context);
 
-
 			// Check if an error occurred
 			if ($result === false) {
 			
 				$error = error_get_last();
-//				var_dump($error);
+
+		// Restore the previous error reporting level
+		error_reporting($previous_error_reporting);
 			
 				if ($error !== null) {
 					if(str_contains($error['message'],'503')){
 						$timer += $this->slUpdate;
 						echo "Model not loaded yet, trying again in $timer seconds\n";
+						if($this->logAll) $this->logString("Main: Model not loaded yet, trying again in $timer seconds\n") ;
 						$result="";
 						continue;
 					}elseif(str_contains($error['message'],'500')){
 						echo "Server error 500, returning.\n";
-						return;
+						if($this->logAll) $this->logString("Main: Server error 500, returning.\n") ;
+						return 500;
 					}elseif((str_contains($error['message'],'429')) || (str_contains($error['message'],'403')) ){
-						echo "Too many request. Returning.";
-						return;
+						echo "Too many request. Returning.\n";
+						if($this->logAll) $this->logString("Main: Too many request. Returning.\n") ;
+						return 429;
 					}else{
 						$timer += 30;
 						$granate = explode(":",$error['message']);
-						echo $granate[5] ." not acted on.\n\n";
-						return;
+						echo trim($granate[5]) ." not acted on.\n";
+						if($this->logAll) $this->logString("Main: ". trim($granate[5]) . " not acted on.\n") ;
+						return 254;
 					}
 				} else {
 					echo "An unknown error occurred while fetching the webpage. Please try again!\n";
-					return;
+					if($this->logAll) $this->logString("Main: An unknown error occurred while fetching the webpage. Please try again!\n") ;
+					return 255;
 				}
 			}
 			break; // we have a picture
 		} //end while
 		
-		// Restore the previous error reporting level
-		error_reporting($previous_error_reporting);
 
-		// Decode the image blob
-		$image = new Imagick();
-		$image->readImageBlob($result);
-		$image->setImageFormat('png');
-		$id = $this->imgStore.'/'.$this->sName.date('jmdHms').'.png';
-		$image->writeImage($id);
+		// Write the image.
 		
-		if($this->exiv2) shell_exec('exiv2 -M"set Exif.Image.ImageDescription Prompt: '. $aiMessage ."\n\n Neg: ".Huggingface::NEGATIVE.'" '.$id);
-
-		echo "\nImage stored as $id\n";
+		$this->writeImage($result);
+		
+		return 0;
 
 	} 
 	
@@ -282,7 +311,7 @@ echo "\n\nEndpoint short name: ".$this->sName."\n\n";
 	private function help(){
 echo'
 Commands:
-
+k
 /helpme      
 This help
 
@@ -291,9 +320,6 @@ Leave class
 
 /setmodel  <model>
 Set model to shortname of model
-
-/savemodels
-Savemodels
 
 /addmodel <short> <model>
 Addmodel choose a shortname and add Hugging model
@@ -305,7 +331,7 @@ Just click on the copy icon in Hugging
 Delete model with sortname
 
 /getmodels
-load the models from file
+load the original models from file
 
 /listmodels
 Listmodels
@@ -313,7 +339,167 @@ Listmodels
 /models2txt
 Creates a txt list of models, easy if you want to keep notes.
 
+/txt2models <textfile
+replace current models by <textfile> full path name.
+
 ';
 	}
+	
+	function addModel($input){
+
+		$granate = explode( " " , $input );
+
+		if(! array_key_exists($granate[0],$this->models)){
+
+			$smodel = array_search($granate[1], $this->models);
+
+			if(! $smodel){
+
+				$this->models[$granate[0]] = $granate[1];
+
+			}else{
+
+				echo "\nModel already available as $smodel\n";
+				return;
+
+			}
+
+		}else{
+
+			echo "\nShortname already in use!\n";
+			return;
+
+		}
+
+		echo "\nModel $input added\n";
+
+		return;
+	}
+	
+	function getModels($input){
+		            
+		if(strlen($input) == 10){
+			
+			$name = "models"; // default models
+
+		}else{
+
+			$name = substr($input,11);
+
+		}
+
+		$id = __DIR__."/$name.json";
+		
+		if(! is_file( $id )){
+
+			echo "\nModel $name not found\n";
+			
+			return;
+			
+		}
+
+		$this->models = json_decode(file_get_contents($id), true);
+		echo "\nModels loaded\n";
+
+	}
+	function loopModels($prompt){
+		
+		
+		//store current endPoint.
+		$storeEndpoint = $this->endPoint;
+		$storeSname = $this->sName;
+	
+		foreach( $this->models as $key => $model ) {
+			$response="";
+
+			//set endpoint
+			$this->endPoint = Huggingface::INFERENCE.$model;
+			$this->sName = $key;
+			
+			$response = $this->apiCompletion($prompt);
+
+			if($response == 429){
+				echo "\nStopping an hour due to API ratelimiting, sorry.\n";
+				if($this->logAll) $this->logString("Loop:Stopping an hour due to API ratelimiting, sorry.\n") ;
+				sleep(3600);
+			}elseif($response == 500){
+				echo "\nModel skipped due to server error, sorry.\n";
+				if($this->logAll) $this->logString("Loop: Model skipped due to server error, sorry.\n") ;
+			}elseif(! $response == "0"){
+				echo "\nModel skipped due to $response, sorry.\n";
+				if($this->logAll) $this->logString("Loop: Model skipped due to $response, sorry.\n"); ;
+			}elseif( $response == $this->slMax){
+				echo "\nModel skipped due to sleeptimeout, sorry.\n";
+				if($this->logAll) $this->logString("Loop: Model skipped due to sleeptimeout, sorry.\n");
+			}
+
+		}
+		
+		// restore endPoint
+		$this->endPoint = $storeEndpoint;
+		$this->sName = $storeSname;
+		
+	}
+	function logString($string){
+		
+		$log = $this->imgStore.'/log.txt';
+		
+		file_put_contents( $log , date("Y-m-d H:i:s") . " " . $string , FILE_APPEND );
+		
+		return;
+	}
+
+	function importModels($string){
+		
+		if( ! is_file( $string )) {
+			echo "$string - NOT FOUND\n";
+			return;
+		}else{
+			$stack = file_get_contents($string);
+			$lines = explode( "\n" , $stack );
+		}
+		
+		foreach( $lines as $line ){
+			
+			if( ! $line == "" ){
+				echo "Trying to add: $line\n";
+				$this->addModel($line);
+			}
+		}
+		
+		return;
+	}
+	
+	private function setExif($aiMessage,$id){
+		
+		$myM =  '-M"set Exif.Image.ImageDescription '."\nPrompt: ". $this->prompt ."\n\nNeg: ".Huggingface::NEGATIVE.'"';
+		$myM .= ' -M"set Xmp.plus.ImageSupplierName '.$this->exiv2User.'"';
+		$myM .= ' -M"set Xmp.dc.creator '.$this->exiv2User.'"';
+		$myM .= ' -M"set Xmp.dc.rights '.$this->exiv2Copy.'"';
+		$myM .= ' -M"set Xmp.photoshop.Credit '.$this->exiv2User.'"';
+		$myM .= ' -M"set Xmp.xmp.CreatorTool clsHuggingface"';
+		$myM .= ' -M"set Exif.Image.Software clsHuggingface"';
+		$myM .= ' -M"set Exif.Photo.UserComment '.$this->endPoint.'"';
+		shell_exec("exiv2 $myM ".$id);
+
+	}
+	
+	private function writeImage($blob){
+		
+		$image = new Imagick();
+		$image->readImageBlob($blob);
+		$image->setImageFormat('png');
+		$id = $this->imgStore.'/'.$this->sName.'-'.date('jmdHms').'.png';
+		$image->writeImage($id);
+		
+		if($this->exiv2) $this->setExif($aiMessage,$id);
+		
+		echo "\nImage stored as $id\n";
+		if($this->logAll) $this->logString("Main: Image stored as $id\n") ;
+		return 0;
+
+	}
+	
+	
 }
 ?>
